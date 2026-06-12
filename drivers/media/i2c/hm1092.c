ee252e40e1b97cb3cefbe721d223e1facf176eb1
Ramshouriesh R <rshouriesh@gmail.com>
2026-06-12 23:07:12 +0530

    media: i2c: hm1092: add Himax HM1092 mono NIR sensor driver
    Add a V4L2 subdev driver for the Himax HM1092, a 1 megapixel monochrome
    near-infrared image sensor with a single-lane MIPI CSI-2 interface,
    typically used as the face-authentication IR camera on laptops.
    
    The driver supports the sensor's native 560x360 10-bit mono mode, a test
    pattern control and the standard fwnode properties (orientation,
    rotation).
    
    Signed-off-by: Ramshouriesh R <rshouriesh@gmail.com>
    (cherry picked from commit 505cb399587b71a8ae9b0c45cd505ae02919f0c2)

--- drivers/media/i2c/hm1092.c
+++ drivers/media/i2c/hm1092.c
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Himax HM1092 image sensor driver draft.
+ * Himax HM1092 monochrome near-infrared image sensor driver.
  *
- * Register tables were extracted from Qualcomm Chromatix sensor module
- * com.qti.sensormodule.hm1092.bin. Keep hm1092_regs.h next to this file, or
- * fold the generated tables into this source before upstream submission.
+ * Copyright (c) 2026 Ramshouriesh R <rshouriesh@gmail.com>
  */
 
 #include <linux/clk.h>
@@ -19,13 +17,218 @@
 #include <media/v4l2-device.h>
 #include <media/v4l2-fwnode.h>
 
-#include "hm1092_regs.h"
-
 #define HM1092_LINK_FREQ_400MHZ		400000000ULL
 #define HM1092_MCLK			24000000
 #define HM1092_BITS_PER_SAMPLE		10
 
 #define HM1092_REG_STREAM		CCI_REG8(0x0100)
+#define HM1092_STREAM_STANDBY		0x00
+#define HM1092_STREAM_ON		0x01
+
+#define HM1092_REG_TEST_PATTERN		CCI_REG8(0x0601)
+#define HM1092_TEST_PATTERN_MAX		4
+
+/*
+ * Pixel array geometry, as programmed by the streaming init sequence:
+ * the readout window spans X_ADDR [0x0030..0x04ad] and Y_ADDR
+ * [0x0008..0x02d5], i.e. a 1150x718 active area whose bottom-right corner
+ * is the last addressable pixel. There is a single fixed mode that 2x bins
+ * this window down to the 560x360 output.
+ */
+#define HM1092_NATIVE_WIDTH		1198U
+#define HM1092_NATIVE_HEIGHT		726U
+#define HM1092_ACTIVE_LEFT		48U
+#define HM1092_ACTIVE_TOP		8U
+#define HM1092_ACTIVE_WIDTH		1150U
+#define HM1092_ACTIVE_HEIGHT		718U
+
+static const struct cci_reg_sequence hm1092_init_regs[] = {
+	{ CCI_REG8(0x0103), 0x00 },
+	{ CCI_REG8(0x030a), 0x05 },
+	{ CCI_REG8(0x030d), 0x0c },
+	{ CCI_REG8(0x030f), 0x5a },
+	{ CCI_REG8(0x0307), 0x00 },
+	{ CCI_REG8(0x0309), 0x01 },
+	{ CCI_REG8(0x0387), 0x01 },
+	{ CCI_REG8(0x0100), 0x02 },
+	{ CCI_REG8(0x4265), 0x02 },
+	{ CCI_REG8(0x4002), 0x2b },
+	{ CCI_REG8(0x4001), 0x00 },
+	{ CCI_REG8(0x0101), 0x03 },
+	{ CCI_REG8(0x4024), 0x40 },
+	{ CCI_REG8(0x0203), 0xbe },
+	{ CCI_REG8(0x0202), 0x00 },
+	{ CCI_REG8(0x0341), 0xee },
+	{ CCI_REG8(0x0340), 0x02 },
+	{ CCI_REG8(0x0343), 0x50 },
+	{ CCI_REG8(0x0342), 0x06 },
+	{ CCI_REG8(0x0345), 0x30 },
+	{ CCI_REG8(0x0344), 0x00 },
+	{ CCI_REG8(0x0349), 0xad },
+	{ CCI_REG8(0x0348), 0x04 },
+	{ CCI_REG8(0x0347), 0x08 },
+	{ CCI_REG8(0x0346), 0x00 },
+	{ CCI_REG8(0x034b), 0xd5 },
+	{ CCI_REG8(0x034a), 0x02 },
+	{ CCI_REG8(0x5015), 0xb3 },
+	{ CCI_REG8(0x0350), 0x53 },
+	{ CCI_REG8(0x0361), 0x30 },
+	{ CCI_REG8(0x0360), 0x00 },
+	{ CCI_REG8(0x034d), 0x30 },
+	{ CCI_REG8(0x034c), 0x02 },
+	{ CCI_REG8(0x034f), 0x68 },
+	{ CCI_REG8(0x034e), 0x01 },
+	{ CCI_REG8(0x0390), 0x03 },
+	{ CCI_REG8(0x0383), 0x00 },
+	{ CCI_REG8(0x0387), 0x10 },
+	{ CCI_REG8(0x50dd), 0x01 },
+	{ CCI_REG8(0x50cb), 0x21 },
+	{ CCI_REG8(0x5005), 0x28 },
+	{ CCI_REG8(0x5004), 0x40 },
+	{ CCI_REG8(0x5007), 0x28 },
+	{ CCI_REG8(0x5006), 0x40 },
+	{ CCI_REG8(0x5011), 0x00 },
+	{ CCI_REG8(0x501d), 0x4c },
+	{ CCI_REG8(0x5013), 0x03 },
+	{ CCI_REG8(0x4131), 0x01 },
+	{ CCI_REG8(0x5283), 0x03 },
+	{ CCI_REG8(0x5282), 0xff },
+	{ CCI_REG8(0x5010), 0x20 },
+	{ CCI_REG8(0x4132), 0x20 },
+	{ CCI_REG8(0x50d5), 0xe0 },
+	{ CCI_REG8(0x50d7), 0x12 },
+	{ CCI_REG8(0x50bb), 0x14 },
+	{ CCI_REG8(0x50b7), 0x00 },
+	{ CCI_REG8(0x50b9), 0xff },
+	{ CCI_REG8(0x50b8), 0x70 },
+	{ CCI_REG8(0x50ba), 0xff },
+	{ CCI_REG8(0x50fa), 0x02 },
+	{ CCI_REG8(0x50b4), 0x00 },
+	{ CCI_REG8(0x50a2), 0x0b },
+	{ CCI_REG8(0x50ad), 0x07 },
+	{ CCI_REG8(0x50ac), 0x24 },
+	{ CCI_REG8(0x50af), 0x40 },
+	{ CCI_REG8(0x50ae), 0x20 },
+	{ CCI_REG8(0x50ab), 0x07 },
+	{ CCI_REG8(0x50aa), 0x22 },
+	{ CCI_REG8(0x50a7), 0x00 },
+	{ CCI_REG8(0x50a6), 0x00 },
+	{ CCI_REG8(0x5099), 0x11 },
+	{ CCI_REG8(0x509b), 0x03 },
+	{ CCI_REG8(0x50b3), 0x04 },
+	{ CCI_REG8(0x50a0), 0x30 },
+	{ CCI_REG8(0x5098), 0x00 },
+	{ CCI_REG8(0x52f2), 0x53 },
+	{ CCI_REG8(0x5209), 0x0c },
+	{ CCI_REG8(0x5216), 0x02 },
+	{ CCI_REG8(0x521e), 0x01 },
+	{ CCI_REG8(0x50e8), 0x00 },
+	{ CCI_REG8(0x5200), 0x60 },
+	{ CCI_REG8(0x5202), 0x00 },
+	{ CCI_REG8(0x5201), 0x80 },
+	{ CCI_REG8(0x5203), 0x01 },
+	{ CCI_REG8(0x5208), 0x0b },
+	{ CCI_REG8(0x520d), 0x40 },
+	{ CCI_REG8(0x520c), 0x15 },
+	{ CCI_REG8(0x5215), 0x04 },
+	{ CCI_REG8(0x50ea), 0x74 },
+	{ CCI_REG8(0x5214), 0x28 },
+	{ CCI_REG8(0x5218), 0x07 },
+	{ CCI_REG8(0x5217), 0x01 },
+	{ CCI_REG8(0x0310), 0x00 },
+	{ CCI_REG8(0x4b31), 0x06 },
+	{ CCI_REG8(0x4b3b), 0x02 },
+	{ CCI_REG8(0x4b45), 0x01 },
+	{ CCI_REG8(0x4b44), 0x0c },
+	{ CCI_REG8(0x4b47), 0x00 },
+	{ CCI_REG8(0x5101), 0x13 },
+	{ CCI_REG8(0x5100), 0x03 },
+	{ CCI_REG8(0x5103), 0x33 },
+	{ CCI_REG8(0x5102), 0x23 },
+	{ CCI_REG8(0x5105), 0x42 },
+	{ CCI_REG8(0x5104), 0x43 },
+	{ CCI_REG8(0x5106), 0x40 },
+	{ CCI_REG8(0x5119), 0x00 },
+	{ CCI_REG8(0x5118), 0x00 },
+	{ CCI_REG8(0x511b), 0x00 },
+	{ CCI_REG8(0x511a), 0x00 },
+	{ CCI_REG8(0x511d), 0x00 },
+	{ CCI_REG8(0x511c), 0x00 },
+	{ CCI_REG8(0x511e), 0x00 },
+	{ CCI_REG8(0x5131), 0x23 },
+	{ CCI_REG8(0x5130), 0x13 },
+	{ CCI_REG8(0x5133), 0x43 },
+	{ CCI_REG8(0x5132), 0x33 },
+	{ CCI_REG8(0x5135), 0x40 },
+	{ CCI_REG8(0x5134), 0x42 },
+	{ CCI_REG8(0x5136), 0x40 },
+	{ CCI_REG8(0x5149), 0x01 },
+	{ CCI_REG8(0x5148), 0x01 },
+	{ CCI_REG8(0x514b), 0x01 },
+	{ CCI_REG8(0x514a), 0x01 },
+	{ CCI_REG8(0x514d), 0x01 },
+	{ CCI_REG8(0x514c), 0x01 },
+	{ CCI_REG8(0x514e), 0x01 },
+	{ CCI_REG8(0x51c0), 0x00 },
+	{ CCI_REG8(0x51c6), 0x00 },
+	{ CCI_REG8(0x51cc), 0x00 },
+	{ CCI_REG8(0x51d2), 0x00 },
+	{ CCI_REG8(0x51d8), 0x00 },
+	{ CCI_REG8(0x51c1), 0x81 },
+	{ CCI_REG8(0x51c7), 0x81 },
+	{ CCI_REG8(0x51cd), 0x81 },
+	{ CCI_REG8(0x51d3), 0x81 },
+	{ CCI_REG8(0x51d9), 0x81 },
+	{ CCI_REG8(0x51c2), 0xec },
+	{ CCI_REG8(0x51c8), 0xec },
+	{ CCI_REG8(0x51ce), 0xec },
+	{ CCI_REG8(0x51d4), 0xec },
+	{ CCI_REG8(0x51da), 0xec },
+	{ CCI_REG8(0x51c3), 0x00 },
+	{ CCI_REG8(0x51c9), 0x00 },
+	{ CCI_REG8(0x51cf), 0x00 },
+	{ CCI_REG8(0x51d5), 0x00 },
+	{ CCI_REG8(0x51db), 0x00 },
+	{ CCI_REG8(0x51c4), 0x55 },
+	{ CCI_REG8(0x51ca), 0x55 },
+	{ CCI_REG8(0x51d0), 0x54 },
+	{ CCI_REG8(0x51d6), 0x53 },
+	{ CCI_REG8(0x51dc), 0x53 },
+	{ CCI_REG8(0x51c5), 0x44 },
+	{ CCI_REG8(0x51cb), 0x24 },
+	{ CCI_REG8(0x51d1), 0x24 },
+	{ CCI_REG8(0x51d7), 0x14 },
+	{ CCI_REG8(0x51dd), 0x14 },
+	{ CCI_REG8(0x51e0), 0x09 },
+	{ CCI_REG8(0x51e2), 0x04 },
+	{ CCI_REG8(0x51e4), 0x08 },
+	{ CCI_REG8(0x51e6), 0x08 },
+	{ CCI_REG8(0x51e1), 0x03 },
+	{ CCI_REG8(0x51e3), 0x03 },
+	{ CCI_REG8(0x51e5), 0x07 },
+	{ CCI_REG8(0x51e8), 0x04 },
+	{ CCI_REG8(0x51e7), 0x07 },
+	{ CCI_REG8(0x51e9), 0x46 },
+	{ CCI_REG8(0x51eb), 0x62 },
+	{ CCI_REG8(0x51ea), 0x43 },
+	{ CCI_REG8(0x51ed), 0x00 },
+	{ CCI_REG8(0x51ec), 0x61 },
+	{ CCI_REG8(0x51ee), 0x00 },
+	{ CCI_REG8(0x5206), 0x80 },
+	{ CCI_REG8(0x3110), 0x02 },
+	{ CCI_REG8(0x3704), 0x02 },
+	{ CCI_REG8(0x3704), 0x02 },
+	{ CCI_REG8(0x4b20), 0x9e },
+	{ CCI_REG8(0x4b18), 0x00 },
+	{ CCI_REG8(0x4b3e), 0x00 },
+	{ CCI_REG8(0x4b0e), 0x0e },
+	{ CCI_REG8(0x4800), 0xac },
+	{ CCI_REG8(0x0104), 0x01 },
+	{ CCI_REG8(0x0104), 0x00 },
+	{ CCI_REG8(0x4801), 0xae },
+	{ CCI_REG8(0x0000), 0x00 },
+	{ CCI_REG8(0x0037), 0x30 },
+};
 
 struct hm1092_mode {
 	u32 width;
@@ -47,12 +250,17 @@ static const char * const hm1092_supply_names[] = {
 	"dvdd",
 };
 
+/*
+ * The available HM1092 documentation does not describe the test patterns.
+ * Their names are based on observed output from hardware testing; the
+ * register and mode values were reverse-engineered from the Windows driver.
+ */
 static const char * const hm1092_test_pattern_menu[] = {
 	"Disabled",
-	"Mode 1",
-	"Mode 2",
-	"Mode 3",
-	"Mode 4",
+	"Solid Color Fill",
+	"Standard Color Bars",
+	"Fade To Grey Color Bars",
+	"Pseudorandom data",
 };
 
 static const s64 hm1092_link_freq_menu[] = {
@@ -80,64 +288,23 @@ static inline struct hm1092 *to_hm1092(struct v4l2_subdev *sd)
 	return container_of(sd, struct hm1092, sd);
 }
 
-static int hm1092_write_regs(struct hm1092 *hm1092,
-			     const struct hm1092_reg *regs, unsigned int len)
-{
-	int ret = 0;
-	unsigned int i;
-
-	for (i = 0; i < len; i++) {
-		cci_write(hm1092->regmap, CCI_REG8(regs[i].address),
-			  regs[i].val, &ret);
-		if (ret)
-			return ret;
-	}
-
-	return 0;
-}
-
-static int hm1092_set_test_pattern(struct hm1092 *hm1092, int pattern)
-{
-	switch (pattern) {
-	case 0:
-		return hm1092_write_regs(hm1092, hm1092_test_pattern_mode0,
-					 ARRAY_SIZE(hm1092_test_pattern_mode0));
-	case 1:
-		return hm1092_write_regs(hm1092, hm1092_test_pattern_mode1,
-					 ARRAY_SIZE(hm1092_test_pattern_mode1));
-	case 2:
-		return hm1092_write_regs(hm1092, hm1092_test_pattern_mode2,
-					 ARRAY_SIZE(hm1092_test_pattern_mode2));
-	case 3:
-		return hm1092_write_regs(hm1092, hm1092_test_pattern_mode3,
-					 ARRAY_SIZE(hm1092_test_pattern_mode3));
-	case 4:
-		return hm1092_write_regs(hm1092, hm1092_test_pattern_mode4,
-					 ARRAY_SIZE(hm1092_test_pattern_mode4));
-	default:
-		return -EINVAL;
-	}
-}
-
 static int hm1092_set_ctrl(struct v4l2_ctrl *ctrl)
 {
 	struct hm1092 *hm1092 = container_of(ctrl->handler, struct hm1092,
 					     ctrl_handler);
-	int ret = 0;
+	int ret;
 
-	if (!pm_runtime_get_if_in_use(hm1092->dev))
+	/*
+	 * The control value is cached by the framework and (re)applied from
+	 * hm1092_enable_streams() once the device is powered up for streaming.
+	 */
+	if (pm_runtime_get_if_in_use(hm1092->dev) == 0)
 		return 0;
 
 	switch (ctrl->id) {
 	case V4L2_CID_TEST_PATTERN:
-		ret = hm1092_set_test_pattern(hm1092, ctrl->val);
-		break;
-	case V4L2_CID_ANALOGUE_GAIN:
-	case V4L2_CID_EXPOSURE:
-		/* TODO: write to the sensor's exposure/gain registers once
-		 * we know which Chromatix middle*Addr fields point at them.
-		 */
-		ret = 0;
+		ret = cci_write(hm1092->regmap, HM1092_REG_TEST_PATTERN,
+				ctrl->val, NULL);
 		break;
 	default:
 		ret = -EINVAL;
@@ -161,6 +328,10 @@ static int hm1092_init_controls(struct hm1092 *hm1092)
 	s64 hblank, pixel_rate;
 	int ret;
 
+	ret = v4l2_fwnode_device_parse(hm1092->dev, &props);
+	if (ret)
+		return ret;
+
 	v4l2_ctrl_handler_init(ctrl_hdlr, 6);
 
 	hm1092->link_freq = v4l2_ctrl_new_int_menu(ctrl_hdlr,
@@ -168,8 +339,6 @@ static int hm1092_init_controls(struct hm1092 *hm1092)
 						   V4L2_CID_LINK_FREQ,
 						   0, 0,
 						   hm1092_link_freq_menu);
-	if (hm1092->link_freq)
-		hm1092->link_freq->flags |= V4L2_CTRL_FLAG_READ_ONLY;
 
 	pixel_rate = div_u64(HM1092_LINK_FREQ_400MHZ * 2 * hm1092->mipi_lanes,
 			     HM1092_BITS_PER_SAMPLE);
@@ -181,43 +350,37 @@ static int hm1092_init_controls(struct hm1092 *hm1092)
 	hm1092->hblank = v4l2_ctrl_new_std(ctrl_hdlr, &hm1092_ctrl_ops,
 					   V4L2_CID_HBLANK, hblank, hblank, 1,
 					   hblank);
-	if (hm1092->hblank)
-		hm1092->hblank->flags |= V4L2_CTRL_FLAG_READ_ONLY;
 
 	hm1092->vblank = v4l2_ctrl_new_std(ctrl_hdlr, &hm1092_ctrl_ops,
 					   V4L2_CID_VBLANK,
 					   mode->vts - mode->height,
 					   0xffff - mode->height, 1,
 					   mode->vts - mode->height);
-	if (hm1092->vblank)
-		hm1092->vblank->flags |= V4L2_CTRL_FLAG_READ_ONLY;
-
-	/* Mandatory controls for libcamera. Conservative defaults until we
-	 * RE the exposure/gain register address layout from the Chromatix
-	 * sensormodule (middleCoarseIntgTimeAddr / shortGlobalGainAddr).
-	 */
-	v4l2_ctrl_new_std(ctrl_hdlr, &hm1092_ctrl_ops, V4L2_CID_ANALOGUE_GAIN,
-			  0x10, 0xff, 1, 0x10);
-	v4l2_ctrl_new_std(ctrl_hdlr, &hm1092_ctrl_ops, V4L2_CID_EXPOSURE,
-			  1, mode->vts - 4, 1, mode->vts - 4);
 
 	v4l2_ctrl_new_std_menu_items(ctrl_hdlr, &hm1092_ctrl_ops,
 				     V4L2_CID_TEST_PATTERN,
 				     ARRAY_SIZE(hm1092_test_pattern_menu) - 1,
 				     0, 0, hm1092_test_pattern_menu);
 
-	ret = v4l2_fwnode_device_parse(hm1092->dev, &props);
-	if (ret)
-		return ret;
-
 	v4l2_ctrl_new_fwnode_properties(ctrl_hdlr, &hm1092_ctrl_ops, &props);
 
-	if (ctrl_hdlr->error)
-		return ctrl_hdlr->error;
+	if (ctrl_hdlr->error) {
+		ret = ctrl_hdlr->error;
+		goto err_free;
+	}
+
+	hm1092->link_freq->flags |= V4L2_CTRL_FLAG_READ_ONLY;
+	hm1092->hblank->flags |= V4L2_CTRL_FLAG_READ_ONLY;
+	hm1092->vblank->flags |= V4L2_CTRL_FLAG_READ_ONLY;
 
 	hm1092->sd.ctrl_handler = ctrl_hdlr;
 
 	return 0;
+
+err_free:
+	v4l2_ctrl_handler_free(ctrl_hdlr);
+
+	return ret;
 }
 
 static void hm1092_update_pad_format(struct v4l2_mbus_framefmt *fmt)
@@ -226,6 +389,10 @@ static void hm1092_update_pad_format(struct v4l2_mbus_framefmt *fmt)
 	fmt->height = hm1092_mode_560x360.height;
 	fmt->code = MEDIA_BUS_FMT_Y10_1X10;
 	fmt->field = V4L2_FIELD_NONE;
+	fmt->colorspace = V4L2_COLORSPACE_RAW;
+	fmt->ycbcr_enc = V4L2_YCBCR_ENC_DEFAULT;
+	fmt->quantization = V4L2_QUANTIZATION_DEFAULT;
+	fmt->xfer_func = V4L2_XFER_FUNC_NONE;
 }
 
 static int hm1092_enable_streams(struct v4l2_subdev *sd,
@@ -239,8 +406,8 @@ static int hm1092_enable_streams(struct v4l2_subdev *sd,
 	if (ret)
 		return ret;
 
-	ret = hm1092_write_regs(hm1092, hm1092_init_regs,
-				ARRAY_SIZE(hm1092_init_regs));
+	ret = cci_multi_reg_write(hm1092->regmap, hm1092_init_regs,
+				  ARRAY_SIZE(hm1092_init_regs), NULL);
 	if (ret) {
 		dev_err(hm1092->dev, "failed to write init registers\n");
 		goto out;
@@ -250,8 +417,8 @@ static int hm1092_enable_streams(struct v4l2_subdev *sd,
 	if (ret)
 		goto out;
 
-	ret = hm1092_write_regs(hm1092, hm1092_start_streaming,
-				ARRAY_SIZE(hm1092_start_streaming));
+	ret = cci_write(hm1092->regmap, HM1092_REG_STREAM,
+			HM1092_STREAM_ON, NULL);
 	if (ret)
 		dev_err(hm1092->dev, "failed to start streaming\n");
 
@@ -267,22 +434,13 @@ static int hm1092_disable_streams(struct v4l2_subdev *sd,
 				  u32 pad, u64 streams_mask)
 {
 	struct hm1092 *hm1092 = to_hm1092(sd);
-	int ret = 0;
+	int ret;
 
-	cci_write(hm1092->regmap, HM1092_REG_STREAM, 0, &ret);
+	ret = cci_write(hm1092->regmap, HM1092_REG_STREAM,
+			HM1092_STREAM_STANDBY, NULL);
 	pm_runtime_put(hm1092->dev);
 
 	return ret;
-}
-
-static int hm1092_set_format(struct v4l2_subdev *sd,
-			     struct v4l2_subdev_state *state,
-			     struct v4l2_subdev_format *fmt)
-{
-	hm1092_update_pad_format(&fmt->format);
-	*v4l2_subdev_state_get_format(state, fmt->pad) = fmt->format;
-
-	return 0;
 }
 
 static int hm1092_enum_mbus_code(struct v4l2_subdev *sd,
@@ -315,6 +473,30 @@ static int hm1092_enum_frame_size(struct v4l2_subdev *sd,
 	return 0;
 }
 
+static int hm1092_get_selection(struct v4l2_subdev *sd,
+				struct v4l2_subdev_state *state,
+				struct v4l2_subdev_selection *sel)
+{
+	switch (sel->target) {
+	case V4L2_SEL_TGT_CROP:
+	case V4L2_SEL_TGT_CROP_DEFAULT:
+	case V4L2_SEL_TGT_CROP_BOUNDS:
+		sel->r.left = HM1092_ACTIVE_LEFT;
+		sel->r.top = HM1092_ACTIVE_TOP;
+		sel->r.width = HM1092_ACTIVE_WIDTH;
+		sel->r.height = HM1092_ACTIVE_HEIGHT;
+		return 0;
+	case V4L2_SEL_TGT_NATIVE_SIZE:
+		sel->r.left = 0;
+		sel->r.top = 0;
+		sel->r.width = HM1092_NATIVE_WIDTH;
+		sel->r.height = HM1092_NATIVE_HEIGHT;
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
 static int hm1092_init_state(struct v4l2_subdev *sd,
 			     struct v4l2_subdev_state *state)
 {
@@ -328,10 +510,11 @@ static const struct v4l2_subdev_video_ops hm1092_video_ops = {
 };
 
 static const struct v4l2_subdev_pad_ops hm1092_pad_ops = {
-	.set_fmt = hm1092_set_format,
+	.set_fmt = v4l2_subdev_get_fmt,
 	.get_fmt = v4l2_subdev_get_fmt,
 	.enum_mbus_code = hm1092_enum_mbus_code,
 	.enum_frame_size = hm1092_enum_frame_size,
+	.get_selection = hm1092_get_selection,
 	.enable_streams = hm1092_enable_streams,
 	.disable_streams = hm1092_disable_streams,
 };
@@ -352,6 +535,11 @@ static const struct v4l2_subdev_internal_ops hm1092_internal_ops = {
 static int hm1092_check_hwcfg(struct hm1092 *hm1092)
 {
 	struct v4l2_fwnode_endpoint bus_cfg = {
+		.bus = {
+			.mipi_csi2 = {
+				.num_data_lanes = 1,
+			},
+		},
 		.bus_type = V4L2_MBUS_CSI2_DPHY,
 	};
 	struct device *dev = hm1092->dev;
@@ -360,10 +548,6 @@ static int hm1092_check_hwcfg(struct hm1092 *hm1092)
 	int ret;
 
 	ep = fwnode_graph_get_endpoint_by_id(fwnode, 0, 0, 0);
-	if (!ep)
-		return dev_err_probe(dev, -EPROBE_DEFER,
-				     "waiting for fwnode graph endpoint\n");
-
 	ret = v4l2_fwnode_endpoint_alloc_parse(ep, &bus_cfg);
 	fwnode_handle_put(ep);
 	if (ret)
@@ -377,10 +561,9 @@ static int hm1092_check_hwcfg(struct hm1092 *hm1092)
 	if (ret)
 		goto out;
 
-	if (bus_cfg.bus.mipi_csi2.num_data_lanes != 1 &&
-	    bus_cfg.bus.mipi_csi2.num_data_lanes != 2) {
+	if (bus_cfg.bus.mipi_csi2.num_data_lanes != 1) {
 		ret = dev_err_probe(dev, -EINVAL,
-				    "unsupported CSI2 data lanes: %u\n",
+				    "only 1 data lane is supported, got %u\n",
 				    bus_cfg.bus.mipi_csi2.num_data_lanes);
 		goto out;
 	}
@@ -394,15 +577,13 @@ out:
 
 static int hm1092_get_pm_resources(struct hm1092 *hm1092)
 {
-	unsigned int i;
-
 	hm1092->reset = devm_gpiod_get_optional(hm1092->dev, "reset",
 						GPIOD_OUT_HIGH);
 	if (IS_ERR(hm1092->reset))
 		return dev_err_probe(hm1092->dev, PTR_ERR(hm1092->reset),
 				     "failed to get reset gpio\n");
 
-	for (i = 0; i < ARRAY_SIZE(hm1092_supply_names); i++)
+	for (unsigned int i = 0; i < ARRAY_SIZE(hm1092_supply_names); i++)
 		hm1092->supplies[i].supply = hm1092_supply_names[i];
 
 	return devm_regulator_bulk_get(hm1092->dev,
@@ -416,9 +597,9 @@ static int hm1092_power_off(struct device *dev)
 	struct hm1092 *hm1092 = to_hm1092(sd);
 
 	gpiod_set_value_cansleep(hm1092->reset, 1);
+	clk_disable_unprepare(hm1092->img_clk);
 	regulator_bulk_disable(ARRAY_SIZE(hm1092_supply_names),
 			       hm1092->supplies);
-	clk_disable_unprepare(hm1092->img_clk);
 
 	return 0;
 }
@@ -430,8 +611,8 @@ static int hm1092_power_on(struct device *dev)
 	int ret;
 
 	/*
-	 * Sequence reverse-engineered from the Chromatix AeoB powerSetting:
-	 *   1. enable all rails (~1 ms ramp per supply)
+	 * Power-up sequence:
+	 *   1. enable all rails (~3 ms ramp)
 	 *   2. hold reset asserted
 	 *   3. start MCLK and let the sensor clock for ~1 ms
 	 *   4. release reset and wait 18 ms for the sensor to come up
@@ -440,10 +621,9 @@ static int hm1092_power_on(struct device *dev)
 				    hm1092->supplies);
 	if (ret)
 		return ret;
-	usleep_range(3000, 3500);
+	fsleep(3000);
 
-	if (hm1092->reset)
-		gpiod_set_value_cansleep(hm1092->reset, 1);
+	gpiod_set_value_cansleep(hm1092->reset, 1);
 
 	ret = clk_prepare_enable(hm1092->img_clk);
 	if (ret) {
@@ -451,11 +631,10 @@ static int hm1092_power_on(struct device *dev)
 				       hm1092->supplies);
 		return ret;
 	}
-	usleep_range(1000, 1200);
+	fsleep(1000);
 
-	if (hm1092->reset)
-		gpiod_set_value_cansleep(hm1092->reset, 0);
-	msleep(18);
+	gpiod_set_value_cansleep(hm1092->reset, 0);
+	fsleep(18000);
 
 	return 0;
 }
@@ -543,16 +722,20 @@ static int hm1092_probe(struct i2c_client *client)
 		goto err_subdev;
 
 	pm_runtime_idle(hm1092->dev);
+
 	return 0;
 
 err_subdev:
 	pm_runtime_disable(hm1092->dev);
 	pm_runtime_set_suspended(hm1092->dev);
 	v4l2_subdev_cleanup(&hm1092->sd);
+
 err_entity:
 	media_entity_cleanup(&hm1092->sd.entity);
+
 err_ctrls:
 	v4l2_ctrl_handler_free(hm1092->sd.ctrl_handler);
+
 err_power_off:
 	hm1092_power_off(hm1092->dev);
 
@@ -571,7 +754,7 @@ MODULE_DEVICE_TABLE(of, hm1092_of_match);
 static struct i2c_driver hm1092_i2c_driver = {
 	.driver = {
 		.name = "hm1092",
-		.pm = pm_sleep_ptr(&hm1092_pm_ops),
+		.pm = pm_ptr(&hm1092_pm_ops),
 		.of_match_table = hm1092_of_match,
 	},
 	.probe = hm1092_probe,
@@ -580,5 +763,5 @@ static struct i2c_driver hm1092_i2c_driver = {
 
 module_i2c_driver(hm1092_i2c_driver);
 
-MODULE_DESCRIPTION("Himax HM1092 sensor driver draft");
+MODULE_DESCRIPTION("Himax HM1092 monochrome NIR image sensor driver");
 MODULE_LICENSE("GPL");
